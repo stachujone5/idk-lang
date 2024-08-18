7b2f794382fb4ed072f5eeb341bb2eddbc137d1c
Stanislław Jakubowski <sjakubowski3554@gmail.com>
2024-08-18 12:10:10 +0200

    Lexer token full expression

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,8 +2,8 @@
 pub enum Token {
     Illegal,
     Eof,
-    Ident,
-    Int,
+    Ident(String),
+    Int(String),
     Assign,
     Plus,
     Comma,
@@ -48,7 +48,41 @@ impl Lexer {
         self.read_position += 1
     }
 
+    fn is_letter(&self) -> bool {
+        self.ch.is_ascii_alphabetic() || self.ch == b'_'
+    }
+
+    fn is_int(&self) -> bool {
+        self.ch.is_ascii_digit()
+    }
+
+    fn read_number(&mut self) -> String {
+        let position = self.position;
+        while self.is_int() {
+            self.read_char()
+        }
+
+        String::from_utf8_lossy(&self.input[position..self.position]).into()
+    }
+
+    fn read_identifier(&mut self) -> String {
+        let position = self.position;
+        while self.is_letter() {
+            self.read_char()
+        }
+
+        String::from_utf8_lossy(&self.input[position..self.position]).into()
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.ch.is_ascii_whitespace() {
+            self.read_char()
+        }
+    }
+
     fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+
         let t = match self.ch {
             b'=' => Token::Assign,
             b';' => Token::Semicolon,
@@ -58,7 +92,26 @@ impl Lexer {
             b'+' => Token::Plus,
             b'{' => Token::Lbrace,
             b'}' => Token::Rbrace,
-            _ => Token::Illegal,
+            0 => Token::Eof,
+            _ => {
+                if self.is_letter() {
+                    let identifier = self.read_identifier();
+
+                    match identifier.as_str() {
+                        "fn" => return Token::Function,
+                        "let" => return Token::Let,
+                        _ => (),
+                    };
+
+                    return Token::Ident(identifier);
+                }
+
+                if self.is_int() {
+                    return Token::Int(self.read_number());
+                }
+
+                Token::Illegal
+            }
         };
 
         self.read_char();
@@ -84,6 +137,70 @@ mod tests {
             Token::Rbrace,
             Token::Comma,
             Token::Semicolon,
+        ];
+
+        let mut lexer = Lexer::new(input);
+
+        for current_token in tokens {
+            let next_token = lexer.next_token();
+
+            assert_eq!(current_token, next_token);
+        }
+    }
+
+    #[test]
+    fn next_token_full() {
+        let input = String::from(
+            r#"
+     let five = 5;
+     let ten = 10;
+    
+     let add = fn(x, y) {
+         x + y;
+     };
+    
+     let result = add(five, ten);
+         "#,
+        );
+
+        let tokens = vec![
+            Token::Let,
+            Token::Ident(String::from("five")),
+            Token::Assign,
+            Token::Int(String::from("5")),
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident(String::from("ten")),
+            Token::Assign,
+            Token::Int(String::from("10")),
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident(String::from("add")),
+            Token::Assign,
+            Token::Function,
+            Token::Lparen,
+            Token::Ident(String::from("x")),
+            Token::Comma,
+            Token::Ident(String::from("y")),
+            Token::Rparen,
+            Token::Lbrace,
+            Token::Ident(String::from("x")),
+            Token::Plus,
+            Token::Ident(String::from("y")),
+            Token::Semicolon,
+            Token::Rbrace,
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident(String::from("result")),
+            Token::Assign,
+            Token::Ident(String::from("add")),
+            Token::Lparen,
+            Token::Ident(String::from("five")),
+            Token::Comma,
+            Token::Ident(String::from("ten")),
+            Token::Rparen,
+            Token::Semicolon,
+            Token::Eof,
         ];
 
         let mut lexer = Lexer::new(input);
